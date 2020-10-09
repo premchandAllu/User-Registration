48474eae9561e8e7c9022ceb4f2a839e300b5081
premchandAllu <premchandallu1999@gmail.com>
2020-10-09 14:58:37 +0530

    Added: Welcome to User Registration Problem

---
+++ main/java/com/blz/UserRegistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.blz.UserRegistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registraion Problem");
+
+	}
+
+}
