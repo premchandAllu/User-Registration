02595b72404a11bbd568cd778eb1cd8135592abd
premchandAllu <premchandallu1999@gmail.com>
2020-10-10 11:41:09 +0530

    Modified: Tested for all email samples

--- main/java/com/blz/UserRegistration/UserRegistration.java
+++ main/java/com/blz/UserRegistration/UserRegistration.java
@@ -7,44 +7,43 @@ public class UserRegistration {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-	/*	System.out.println("Enter your First Name");
+        
+		System.out.println("Enter your First Name");
 		String firstName = sc.nextLine();
 		boolean fName = firstName.matches("^[A-Z][a-zA-Z]{2,}$");
-
 		// Validating First Name
 		if (fName == true) {
 			System.out.println("Your First Name is : " + firstName);
 		} else {
 			System.out.println("First Name does not match the requirements");
 		}
-
+		
+		
 		System.out.println("Enter your Last Name");
 		String lastName = sc.nextLine();
 		boolean lName = lastName.matches("^[A-Z][a-zA-Z]{2,}$");
-
 		// Validating Last Name
 		if (lName == true) {
 			System.out.println("Your Last Name is : " + lastName);
 		} else {
 			System.out.println("Last Name does not match the requirements");
 		}
-
+		
+		
 		System.out.println("Enter your email");
 		String email = sc.nextLine();
 		boolean e = email.matches("^[a-z]{3,}(.)[a-z]{0,}[@][a-z]{2,}[.][a-z]{2,}(.)[a-z]{0,}$");
-
 		// Validating Email
 		if (e == true) {
 			System.out.println("Your Email is : " + email);
 		} else {
 			System.out.println("Email does not match the requirements");
 		}
-
+		
+		
 		System.out.println("Enter your mobile number");
 		String mobile = sc.nextLine();
 		boolean m = mobile.matches("^[0-9]{2}[' '][6-9][0-9]{9}$");
-
 		// Validating Mobile Number
 		if (m == true) {
 			System.out.println("Your Mobile number is : " + mobile);
@@ -52,10 +51,9 @@ public class UserRegistration {
 			System.out.println("Mobile number does not match the requirements");
 		}
 		
-*/	
+
 		System.out.println("Enter your password");
 		String password = sc.nextLine();
-		sc.close();
 		Boolean p = password.matches("[a-z]{0,}[A-Z]{1,}[a-z]{0,}[@$%*#./?]{1}[0-9]{1,}${8,}");
 
 		// Validating password using Rule 3
@@ -64,7 +62,20 @@ public class UserRegistration {
 		} else {
 			System.out.println("Password does not match the requirements");
 		}
+		
+		
+		System.out.println("Enter your email sample");
+		String emailSample = sc.nextLine();
+		sc.close();
+		boolean e2 = emailSample.matches(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+		// Validating email samples
+		if (e2 == true) {
+			System.out.println("Your email sample is : " + emailSample);
+		} else {
+			System.out.println("email sample does not match the requirements");
+		}
 	}
 
 }
- 
