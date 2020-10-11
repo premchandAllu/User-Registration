9dcebba825a37e0b50c990e8d2fb91ff33c437bd
premchandAllu <premchandallu1999@gmail.com>
2020-10-11 14:35:32 +0530

    Modified:test for multiple user email inputs

--- main/java/com/blz/UserRegistration/UserRegistration.java
+++ main/java/com/blz/UserRegistration/UserRegistration.java
@@ -27,7 +27,7 @@ public class UserRegistration {
 	
 	public boolean checkEmail(String EmailID)
 	 {
-	   boolean e = EmailID.matches ("^[A-Z][a-zA-Z]{2,}$");
+	   boolean e = EmailID.matches ("^[a-z]{3,}(.)[a-z]{0,}[@][a-z]{2,}[.][a-z]{2,}(.)[a-z]{0,}$");
 
      if(e)
       {
@@ -39,7 +39,7 @@ public class UserRegistration {
 	
 	public boolean checkPhoneNumber(String PhoneNo)
 	 {
-	   boolean p = PhoneNo.matches ("^[A-Z][a-zA-Z]{2,}$");
+	   boolean p = PhoneNo.matches ("^[0-9]{2}[' '][6-9][0-9]{9}$");
 
      if(p)
       {
@@ -50,7 +50,7 @@ public class UserRegistration {
 	
 	public boolean checkPassword(String Password)
 	 {
-	   boolean p1 = Password.matches ("^[A-Z][a-zA-Z]{2,}$");
+	   boolean p1 = Password.matches ("[a-z]{0,}[A-Z]{1,}[a-z]{0,}[@$%*#./?]{1}[0-9]{1,}${8,}");
 
     if(p1)
      {
---
+++ test/java/com/blz/UserRegistration/EmailTesting.java
@@ -0,0 +1,30 @@
+package com.blz.UserRegistration;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EmailTesting {
+
+		@Test
+		public void testEmail()
+		{
+			UserRegistration obj = new UserRegistration();
+			assertTrue("Check pattern defined for correct type of Email 1", obj.checkEmail("abc-100@yahoo.com");
+			assertTrue("Check pattern defined for correct type of Email 2", obj.checkEmail("abc@yahoo.com"));
+			assertTrue("Check pattern defined for correct type of Email 3", obj.checkEmail("abc.100@yahoo.com"));
+			assertTrue("Check pattern defined for correct type of Email 4", obj.checkEmail("abc111@yahoo.com"));
+			assertTrue("Check pattern defined for correct type of Email 5", obj.checkEmail("abc-100@abc.net"));
+			assertTrue("Check pattern defined for correct type of Email 6", obj.checkEmail("abc.100@abc.com.au"));
+			assertTrue("Check pattern defined for correct type of Email 7", obj.checkEmail("abc@1.com"));
+			assertTrue("Check pattern defined for correct type of Email 8", obj.checkEmail("abc@gmail.com.com"));
+			assertTrue("Check pattern defined for correct type of Email 9", obj.checkEmail("abc+100@gmail.com"));
+			
+			assertFalse("Check pattern defined for wrong type of Email", obj.checkEmail("abc.xybl.co.in"));
+			assertFalse("Check pattern defined for wrong type of Email", obj.checkEmail("abcd.xyz.bl.co.in"));
+		}
+	}
+
+
+}
